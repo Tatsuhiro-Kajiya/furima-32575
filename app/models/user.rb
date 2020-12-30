77bb9bada7b2b78dcde8404e81cf1a83aeb0b5a7
Tatsuhiro-Kajiya <75005529+Tatsuhiro-Kajiya@users.noreply.github.com>
2020-12-30 12:42:45 +0900

    ユーザー管理機能の修正7

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,13 +7,17 @@ class User < ApplicationRecord
     VALID_PASSWORD_REGEX = /([0-9].*[a-zA-Z]|[a-zA-Z].*[0-9])/
     VALID_NAME_REGEX = /\A[ぁ-んァ-ン一-龥々+\z]/
     VALID_NAME_KANA_REGEX = /\A[ァ-ヶー－]+\z/
-    
+
     validates :nickname
-    validates :password, format: { with: VALID_PASSWORD_REGEX, message: "is invalid. Input Cannot be registered with a mixture of half-width alphanumeric characters " }
-    validates :last_name, format: { with: VALID_NAME_REGEX, message: "is invalid. Input full-width characters." }
-    validates :first_name, format: { with: VALID_NAME_REGEX, message: "is invalid. Input full-width characters." }
-    validates :last_name_kana, format: { with: VALID_NAME_KANA_REGEX, message: "is invalid. Input full-width katakana characters." }
-    validates :first_name_kana, format: { with: VALID_NAME_KANA_REGEX, message: "is invalid. Input full-width katakana characters." }
     validates :birthday
+    validates :password, format: { with: VALID_PASSWORD_REGEX, message: "is invalid. Input Cannot be registered with a mixture of half-width alphanumeric characters " }
+    with_options format: { with: VALID_NAME_REGEX, message: "is invalid. Input full-width characters." } do
+      validates :last_name
+      validates :first_name
+    end
+    with_options format: { with: VALID_NAME_KANA_REGEX, message: "is invalid. Input full-width katakana characters." } do
+      validates :last_name_kana
+      validates :first_name_kana
+    end
   end
 end
